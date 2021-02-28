7f9b60f70340b286b5c5db65506e94a2233c4567
Pavlushka187 <79157325+Pavlushka187@users.noreply.github.com>
2021-02-28 16:14:17 +0300

    Add files via upload

---
+++ Main (1).cs
@@ -0,0 +1,65 @@
+/***************************************************************
+                        Q=-L*m
+***************************************************************/
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+            
+            Console.WriteLine("welcome");
+            Console.WriteLine("ВВедите жидкость для вычисления конденсации ПРИМЕР (Вода)");
+             string жидкость=(Console.ReadLine());
+            Console.WriteLine("ВВедите массу жидкости");
+            int m = Convert.ToInt32(Console.ReadLine());
+            
+            if (жидкость = Вода)
+            {
+                double z = m*3.2;
+                    Console.WriteLine($"Q: {z}");
+            }
+            /********************************************************************************/
+            else
+            {
+                if (жидкость = Ртуть)
+                {
+                    z = m*0.3;
+                    Console.WriteLine($"Q: {z}");
+                }
+                    else
+                    {
+                        if (жидкость = Кислород)
+                        {
+                            z = m*0.2;
+                            Console.WriteLine($"Q: {z}");
+                            
+                        }
+                        else
+                        {
+                            if (жидкость = Воздух)
+                            {
+                                z = m*0.2;
+                                Console.WriteLine($"Q: {z});
+                            }
+                            else
+                            {
+                                if (жидкость = Спирт)
+                                {
+                                    z = m*0.9;
+                                    Console.WriteLine($"Q: {z}");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("error: не корректное название жидкости");
+                                }
+                            }
+                        }
+                    }
+                }       
+                
+              } 
+        }
+    }
+}
+
+}
