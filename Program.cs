8fb1b7003f97c4cfc79fb294d3c9ada9910af1de
Pavlushka187 <79157325+Pavlushka187@users.noreply.github.com>
2021-03-04 12:08:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/* не (не А и не В)и А
+* не (не А или не В) или А
+ * не (не А или не В) и В */
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            
+            bool a;
+            bool b;
+            Console.WriteLine("Enter A");
+             a = Convert.ToBoolean(Console.ReadLine());
+            byte byteValue;
+            byteValue = Convert.ToByte(a);
+            Console.WriteLine("Enter B");
+            b = Convert.ToBoolean(Console.ReadLine());
+            byteValue = Convert.ToByte(b);
+            Console.WriteLine("Значение равно: ", a, byteValue);
+            Console.Write((!(!a && !b)) && a);
+            Console.ReadKey();
+        }
+  
+    }
+}
